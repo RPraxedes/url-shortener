9d0e70a46813a50de15c6559786445d361aebeb5
RPraxedes <richard0praxedes@gmail.com>
2020-09-30 02:59:59 +0800

    Remove destroy url method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :urls, only: %i[index show new create destroy]
+  resources :urls, only: %i[index show new create]
 
   get "/:id", to: "home#redirect"
   get "/", to: "urls#new"
