5b81d908f031fa5bc93991e266be84a170472fcb
RPraxedes <richard0praxedes@gmail.com>
2020-09-29 18:12:12 +0800

    Add rspec and url model specs

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of :link }
+  end
+
+  describe "before_validation" do
+    it "generate_shortlink" do
+      url = described_class.new(link: "https://en.wikipedia.org/wiki/Special:Random")
+
+      url.save
+
+      expect(url.shortlink).not_to be_nil
+    end
+  end
 end
