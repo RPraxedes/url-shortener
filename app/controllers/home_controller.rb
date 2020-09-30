c9134140ef13b62ced02089f5d916ebfb67d07b8
RPraxedes <richard0praxedes@gmail.com>
2020-09-30 19:58:22 +0800

    Add visitor entry creation using browser gem for info retrieval

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,21 @@ class HomeController < ApplicationController
   def redirect
     shortlink = Url.find_by(shortlink: params[:id])
 
-    redirect_to shortlink.link
+    if shortlink.nil?
+      render "home/error"
+    else
+      visitor = Visitor.create(
+        ip_address: request.remote_ip,
+        browser: "#{browser.name} #{browser.full_version}",
+        platform: "#{browser.platform.name} #{browser.platform.version}",
+        device: browser.device.unknown? ? "computer" : browser.device.name,
+        region: browser.accept_language.first.region,
+        url: shortlink
+      )
+
+      shortlink.visitors << visitor
+
+      redirect_to shortlink.link
+    end
   end
 end
