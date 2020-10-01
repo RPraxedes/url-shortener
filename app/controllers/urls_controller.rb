7e75bc6eaf62807485059749c05b6e6240461af3
RPraxedes <richard0praxedes@gmail.com>
2020-10-01 17:01:50 +0800

    Add queries and table views for daily and hourly visitors

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,7 +5,13 @@ class UrlsController < ApplicationController
     @urls = Url.all
   end
 
-  def show; end
+  def show
+    visitors = @url.visitors
+    @dates = visitors.order("created_at DESC").pluck(:created_at).map(&:to_date).uniq
+    date = params[:date].nil? ? @dates.first : params[:date].to_date
+    @visitors_by_date = visitors.where("DATE(created_at) = ?", date)
+    @visitor_count_by_hour = @visitors_by_date.group_by_hour_of_day(:created_at, format: "%-l %P").count
+  end
 
   def new; end
 
